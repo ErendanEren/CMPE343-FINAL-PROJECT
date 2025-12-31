655a7c5d1f8382eadcfad64998cd845899f68173
dulgerarda <dulgerrarda@gmail.com>
2025-12-31 19:11:55 +0300

    OrderDao and AuthService update

--- src/Service/AuthService.java
+++ src/Service/AuthService.java
@@ -1,4 +1,30 @@
 package Service;
 
+import Models.User;
+import Database.DatabaseConnection;
+import java.sql.*;
+
 public class AuthService {
+    public User login(String username, String password) {
+        String sql = "SELECT * FROM Users WHERE username = ? AND password = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    User user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setUsername(rs.getString("username"));
+                    user.setRole(rs.getString("role")); // Role sütunu önemli
+                    return user;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
